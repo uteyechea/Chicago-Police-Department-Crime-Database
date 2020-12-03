45a7414506de8b7f9eb71c8859db5b70fda70355
uteyechea <uteyechea@gmail.com>
2020-12-02 17:57:30 -0700

    no redundancy

--- DB_Model.sql
+++ DB_Model.sql
@@ -1,55 +1,111 @@
-create database dbRadar
-use dbRadar
+CREATE DATABASE chicago_crime
+USE chicago_crime
 
-create table locationDescription(
-locationDescription_id int primary key,
-description varchar(100),
-domestic bit
+CREATE TABLE location_detail(
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	location_description VARCHAR(150) NOT NULL,
+	block VARCHAR(150) NOT NULL,
+	district INT NOT NULL,
+	ward INT NOT NULL,
+	beat INT NOT NULL,
+	community_area INT NOT NULL
 )
-insert into locationDescription(locationDescription_id,description,domestic)
-select ID,Location_Description,Domestic from chicago_crime_data
 
+INSERT INTO location_detail(location_description,block,district,ward,beat,community_area)
+SELECT DISTINCT Location_Description,Block,District,Ward,Beat,Community_Area FROM dbRadar.dbo.chicago_crime_data
 --Was the insertion successful? compare that all record where moved
-select * from locationDescription 
-select top 100 * from chicago_crime_data
+select * from location_detail --558,308 records 
+--select top 10 Date from chicago_crime_data order by ID desc
 
 
-create table location(
-location_id int primary key,
-block varchar(50),
-locationDescription_id int foreign key(locationDescription_id) references locationDescription(locationDescription_id)
+CREATE TABLE location(
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	x_coordinate INT NOT NULL,
+	y_coordinate INT NOT NULL,
+	latitude DECIMAL(11,9) NOT NULL,
+	longitude DECIMAL(12,9) NOT NULL,
+	location_detail_id INT FOREIGN KEY REFERENCES location_detail(id) NOT NULL
 )
-insert into location(location_id,block,locationDescription_id)
-select ID,block,locationDescription_id from chicago_crime_data,locationDescription
-where chicago_crime_data.ID = locationDescription.locationDescription_id
+
+INSERT INTO location(x_coordinate,y_coordinate,latitude,longitude,location_detail_id)
+SELECT DISTINCT x_coordinate,y_coordinate,latitude,longitude,ld.id 
+FROM dbRadar.dbo.chicago_crime_data as ccd, location_detail as ld
+WHERE 
+	ld.location_description = ccd.Location_Description AND
+	ld.block = ccd.Block AND
+	ld.district = ccd.District AND
+	ld.ward = ccd.Ward AND 
+	ld.beat = ccd.Beat AND
+	ld.community_area = ccd.Community_Area 
 --Was the insertion successful?
-select * from location order by location_id desc 
+SELECT * FROM LOCATION ORDER BY location_detail_id DESC 
 
 
-create table crimeDescription(
-crimeDescription_id int primary key,
-description varchar(100),
-arrest bit
+CREATE TABLE law_enforcement_code(
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	FBI_code VARCHAR(16) NOT NULL,
+	IUCR VARCHAR(8) NOT NULL
 )
-insert into crimeDescription(crimeDescription_id,description,arrest)
-select ID,Description,Arrest from chicago_crime_data
---Was the insertion successful?
-select * from crimeDescription order by crimeDescription_id desc 
+
+INSERT INTO law_enforcement_code(FBI_code,IUCR)
+SELECT DISTINCT FBI_Code,IUCR FROM dbRadar.dbo.chicago_crime_data
+SELECT count(DISTINCT IUCR) FROM law_enforcement_code
 
 
-create table Crime(
-crime_id int primary key identity(1,1),
-date datetime,
-primaryType varchar(50),
-crimeDescription_id int foreign key(crimeDescription_id) references crimeDescription(crimeDescription_id),
-location_id int foreign key(location_id) references location(location_id)
+CREATE TABLE crime_description(
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	description	VARCHAR(150) NOT NULL,
+	updated_on DATETIME NOT NULL,
+	arrest BIT NOT NULL,
+	domestic BIT NOT NULL
 )
-insert into Crime(date,primaryType,crimeDescription_id,location_id)
-select Date,Primary_Type,crimeDescription_id,location_id
-from chicago_crime_data,crimeDescription,location
-where crimeDescription_id=chicago_crime_data.ID and
-location_id = chicago_crime_data.ID
 
-select top 100 * from crime order by crime_id desc
+INSERT INTO crime_description(description,updated_on,arrest,domestic)
+SELECT DISTINCT Description,Updated_On,Arrest,Domestic 
+FROM dbRadar.dbo.chicago_crime_data AS ccd
+
+SELECT COUNT(*) FROM crime_description --151,181 rows_tally
+
+
+CREATE TABLE crime(
+id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+case_number VARCHAR(16) NOT NULL,
+primary_type VARCHAR(50) NOT NULL,
+date DATETIME NOT NULL,
+crime_description_id INT FOREIGN KEY REFERENCES crime_description(id),
+law_enforcement_code_id INT FOREIGN KEY REFERENCES law_enforcement_code(id),
+location_id INT FOREIGN KEY REFERENCES location(id)
+)
+
+INSERT INTO crime(case_number,primary_type,date,crime_description_id,law_enforcement_code_id,location_id)
+SELECT DISTINCT ccd.Case_Number,ccd.Primary_Type,ccd.Date,cd.id,lec.id,l.id 
+FROM dbRadar.dbo.chicago_crime_data as ccd, crime_description as cd, law_enforcement_code as lec, location as l, location_detail as ld
+	WHERE 
+	cd.description	= ccd.Description AND
+	cd.updated_on = ccd.Updated_On AND
+	cd.arrest = ccd.Arrest AND
+	cd.domestic = ccd.Domestic AND
+
+	lec.FBI_code = ccd.FBI_Code AND
+	lec.IUCR = ccd.IUCR AND
+
+	l.x_coordinate = ccd.X_Coordinate AND
+	l.y_coordinate = ccd.Y_Coordinate AND
+	l.latitude = ccd.Latitude AND
+	l.longitude = ccd.Longitude AND
+
+	ld.location_description = ccd.Location_Description AND
+	ld.block = ccd.Block AND
+	ld.district = ccd.District AND
+	ld.ward = ccd.Ward AND 
+	ld.beat = ccd.Beat AND
+	ld.community_area = ccd.Community_Area AND
+	ld.id = l.location_detail_id
+	
+
+SELECT count(*) FROM crime 
+WHERE year(Date)='2018'
+
+
 
 
